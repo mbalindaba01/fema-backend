1272817c1515a4560822594de80ad7aa5b66a6d9
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-07-11 01:09:37 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const route = require('./routes/femaRoutes')
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/fema', route)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log('Server running at port ' + PORT))
