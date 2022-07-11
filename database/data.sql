1f907c7b29ed2c377d230303eb14038612f8e03b
siweh <sanemadesi@gmail.com>
2022-07-12 01:41:33 +0200

    Able to get services and added a table to add service description

---
+++ database/data.sql
@@ -0,0 +1,7 @@
+-- insert into facilities(facility_name, facility_location, facility_reg, facility_capacity, facility_contacno, facility_email, services)
+-- values('bara medi clinic', 'baragwanath, soweto', 'Gp 133 33', 2, '0734553355', 'baragmail.com', '{"Contraceptives", "termination", "family planning"}');
+
+-- insert into facilities(facility_name, facility_location, facility_reg, facility_capacity, facility_contacno, facility_email, services)
+-- values('clicks', 'clicks, joburg', 'GP 334 22', 3, '0737654455', 'clicksgmail.com', '{"Contraceptives", "termination", "family planning", "pregnancy", "menstruation"}');
+
+insert into services(servicename, service_description) values('contraceptives', 'Contraception is a way to prevent pregnancy. There are many different types of contraception and some are more effective than others.')
