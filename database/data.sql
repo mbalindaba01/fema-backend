adb6e2aaa3bf1d02b1560e7e4e896d31ca02477f
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-07-13 12:44:25 +0200

    changed / get route to test

--- database/data.sql
+++ database/data.sql
@@ -4,6 +4,6 @@ values('clicks', 'suninghill', 'Gp 133 33', 2, '0734553355', 'clicksgmail.com', 
 -- insert into facilities(facility_name, facility_location, facility_reg, facility_capacity, facility_contacno, facility_email, services)
 -- values('clicks', 'clicks, joburg', 'GP 334 22', 3, '0737654455', 'clicksgmail.com', '{"Contraceptives", "termination", "family planning", "pregnancy", "menstruation"}');
 
-insert into services(servicename, service_description) values('contraceptives', 'Contraception is a way to prevent pregnancy. There are many different types of contraception and some are more effective than others.')
-insert into services(servicename, service_description) values('pregnancy termination', 'Pregnancy termination is the process of removing a foetus from the body of the pregnant person.')
-insert into services(servicename, service_description) values('ante-natal care', 'Antenatal care is the sufhauif hjgdsyugsudh hdgfyufsduh hgdfyuguisdh hgdfufgusdh')
+insert into services(servicename, service_description) values('contraceptives', 'Contraception is a way to prevent pregnancy. There are many different types of contraception and some are more effective than others.');
+insert into services(servicename, service_description) values('pregnancy termination', 'Pregnancy termination is the process of removing a foetus from the body of the pregnant person.');
+insert into services(servicename, service_description) values('ante-natal care', 'Antenatal care is the sufhauif hjgdsyugsudh hdgfyufsduh hgdfyuguisdh hgdfufgusdh');
