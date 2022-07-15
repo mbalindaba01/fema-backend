75af1a8210aecb0cae4b2e41fafaa831722cb2ad
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-07-15 15:41:58 +0200

    created route to return facilities that matcha service

--- database/data.sql
+++ database/data.sql
@@ -1,9 +1,9 @@
-insert into facilities(facility_name, facility_location, facility_reg, facility_capacity, facility_contacno, facility_email, services_ids, password)
-values('clicks', 'suninghill', 'Gp 133 33', 2, '0734553355', 'clicksgmail.com', '{1, 3}', 'Clicks123');
+insert into facilities(facility_name, facility_location, facility_reg, facility_capacity, facility_contacno, facility_email, password)
+values('clicks', 'suninghill', 'Gp 133 33', 2, '0734553355', 'clicksgmail.com', 'Clicks123');
 
 -- insert into facilities(facility_name, facility_location, facility_reg, facility_capacity, facility_contacno, facility_email, services)
 -- values('clicks', 'clicks, joburg', 'GP 334 22', 3, '0737654455', 'clicksgmail.com', '{"Contraceptives", "termination", "family planning", "pregnancy", "menstruation"}');
 
-insert into services(servicename, service_description) values('contraceptives', 'Contraception is a way to prevent pregnancy. There are many different types of contraception and some are more effective than others.');
-insert into services(servicename, service_description) values('pregnancy termination', 'Pregnancy termination is the process of removing a foetus from the body of the pregnant person.');
-insert into services(servicename, service_description) values('ante-natal care', 'Antenatal care is the sufhauif hjgdsyugsudh hdgfyufsduh hgdfyuguisdh hgdfufgusdh');
+insert into service_config(name, description) values('contraceptives', 'Contraception is a way to prevent pregnancy. There are many different types of contraception and some are more effective than others.');
+insert into service_config(name, description) values('pregnancy termination', 'Pregnancy termination is the process of removing a foetus from the body of the pregnant person.');
+insert into service_config(name, description) values('ante-natal care', 'Antenatal care is the sufhauif hjgdsyugsudh hdgfyufsduh hgdfyuguisdh hgdfufgusdh');
